a5564dbdc883fca194340fec716a501a671487af
Udaypal Singh Dhillon <udaydoesdata@gmail.com>
2024-06-27 11:20:42 +0200

    Query 2

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -18,7 +18,7 @@ FROM
 LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
 WHERE
     job_title_short = 'Data Analyst' AND
-    job_location = 'India' AND
+    job_location = 'Anywhere' AND
     salary_year_avg IS NOT NULL
 ORDER BY
     salary_year_avg DESC
